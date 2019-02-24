7cefb7f3fca3d4159a43f2b5c1fc7f6229fba52e
JohnnySunkel <46906741+JohnnySunkel@users.noreply.github.com>
2019-02-24 11:27:32 -0600

    Create keras_ann_boston_housing.py

---
+++ Keras/keras_ann_boston_housing.py
@@ -0,0 +1,155 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.datasets import boston_housing
+from keras import models
+from keras import layers
+
+# Load the Boston housing dataset
+(train_data, train_targets), (test_data, test_targets) = \
+    boston_housing.load_data()
+    
+# Examine the data
+train_data.shape
+
+test_data.shape
+
+train_targets  
+
+
+# Normalize the data
+mean = train_data.mean(axis = 0)
+train_data -= mean
+std = train_data.std(axis = 0)
+train_data /= std
+
+test_data -= mean
+test_data /= std
+
+
+# Define the model
+def build_model():  
+    model = models.Sequential()
+    model.add(layers.Dense(64, activation = 'relu',
+                           input_shape = (train_data.shape[1],)))
+    model.add(layers.Dense(64, activation = 'relu'))
+    model.add(layers.Dense(1))
+    model.compile(optimizer = 'rmsprop',
+                  loss = 'mse',
+                  metrics = ['mae'])
+    return model
+
+
+# K-fold cross validation with 100 epochs
+k = 4
+num_val_samples = len(train_data) // k
+num_epochs = 100
+all_scores = []
+for i in range(k):
+    print('processing fold #', i)
+    # Prepare the validation data: data from partition # k
+    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+    val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+
+    # Prepare the training data: data from all other partitions
+    partial_train_data = np.concatenate(
+        [train_data[:i * num_val_samples],
+         train_data[(i + 1) * num_val_samples:]],
+         axis = 0)
+    partial_train_targets = np.concatenate(
+        [train_targets[:i * num_val_samples],
+         train_targets[(i + 1) * num_val_samples:]],
+         axis = 0)
+    
+    # Build the Keras model (already compiled)
+    model = build_model()
+    # Train the model (in silent mode, verbose = 0)
+    model.fit(partial_train_data,
+              partial_train_targets,
+              epochs = num_epochs,
+              batch_size = 1,
+              verbose = 0)
+    # Evaluate the model on the validation data
+    val_mse, val_mae = model.evaluate(val_data, val_targets, verbose = 0)
+    all_scores.append(val_mae)
+    
+# Check the results
+all_scores
+np.mean(all_scores)
+
+
+# K-fold cross validation with 500 epochs
+num_epochs = 500
+all_mae_histories = []
+for i in range(k):
+    print('processing fold #', i)
+    # Prepare the validation data: data from partition # k
+    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+    val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+
+    # Prepare the training data: data from all other partitions
+    partial_train_data = np.concatenate(
+        [train_data[:i * num_val_samples],
+         train_data[(i + 1) * num_val_samples:]],
+         axis = 0)
+    partial_train_targets = np.concatenate(
+        [train_targets[:i * num_val_samples],
+         train_targets[(i + 1) * num_val_samples:]],
+         axis = 0)
+    
+    # Build the Keras model (already compiled)
+    model = build_model()
+    # Train the model (in silent mode, verbose = 0)
+    history = model.fit(partial_train_data,
+                        partial_train_targets,
+                        validation_data = (val_data, val_targets),
+                        epochs = num_epochs,
+                        batch_size = 1,
+                        verbose = 0)
+    # Evaluate the model on the validation data
+    mae_history = history.history['val_mean_absolute_error']
+    all_mae_histories.append(mae_history)
+    
+    
+# Build the history of successive mean K-fold validation scores
+average_mae_history = [
+    np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
+    
+ 
+# Plot the validation scores
+plt.plot(range(1, len(average_mae_history) + 1), average_mae_history)
+plt.xlabel('Epochs')
+plt.ylabel('Validation MAE')
+plt.show()  
+
+
+# Remove the first 10 data points and plot again using an
+# exponential moving average to create a smooth curve
+def smooth_curve(points, factor = 0.9):
+    smoothed_points = []
+    for point in points:
+        if smoothed_points:
+            previous = smoothed_points[-1]
+            smoothed_points.append(previous * factor + point * (1 - factor))
+        else:
+            smoothed_points.append(point)
+    return smoothed_points
+
+smooth_mae_history = smooth_curve(average_mae_history[10:])
+
+plt.plot(range(1, len(smooth_mae_history) + 1), smooth_mae_history)
+plt.xlabel('Epochs')
+plt.ylabel('Validation MAE')
+plt.show()
+
+
+# Train the final model and evaluate it on the test data
+model = build_model()
+model.fit(train_data, 
+          train_targets, 
+          epochs = 80,
+          batch_size = 16,
+          verbose = 0)
+
+test_mse_score, test_mae_score = model.evaluate(test_data, test_targets)
+
+test_mae_score
