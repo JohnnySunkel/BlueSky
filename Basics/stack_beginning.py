0a767d7c043c45f8701bc5a67cc8c8cccc6730dc
JohnnySunkel <46906741+JohnnySunkel@users.noreply.github.com>
2019-02-28 14:03:18 -0600

    Create stack_beginning.py

---
+++ Basics/stack_beginning.py
@@ -0,0 +1,20 @@
+# Alternative stack implmentation where the beginning
+# of the list holds the top elements of the stack
+class Stack:
+    def __init__(self):
+        self.items = []
+        
+    def isEmpty(self):
+        return self.items == []
+    
+    def push(self, item):
+        self.items.insert(0, item)
+        
+    def pop(self):
+        return self.items.pop(0)
+    
+    def peek(self):
+        return self.items[0]
+    
+    def size(self):
+        return len(self.items)
