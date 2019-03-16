884fe05629d4a6ce558a6d922bbb31894f2e86a2
JohnnySunkel <46906741+JohnnySunkel@users.noreply.github.com>
2019-03-16 18:20:37 -0500

    Create dutch_flag_partition_faster.py

---
+++ Basics/dutch_flag_partition_faster.py
@@ -0,0 +1,14 @@
+def dutch_flag_partition(pivot_index, A):
+    pivot = A[pivot_index]
+    # First pass - group all elements smaller than the pivot.
+    smaller = 0
+    for i in range(len(A)):
+        if A[i] < pivot:
+            A[i], A[smaller] = A[smaller], A[i]
+            smaller += 1
+    # Second pass - group all elements larger than the pivot.
+    larger = len(A) - 1
+    for i in reversed(range(len(A))):
+        if A[i] > pivot:
+            A[i], A[larger] = A[larger], A[i]
+            larger -= 1
