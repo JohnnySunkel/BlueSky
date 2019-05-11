d6494d9ba9044af3c99edee22a75b6feba3ea2ac
JohnnySunkel <46906741+JohnnySunkel@users.noreply.github.com>
2019-05-11 18:27:03 -0500

    Create pizza_class.py

---
+++ Basics/pizza_class.py
@@ -0,0 +1,24 @@
+import math
+
+class Pizza:
+    def __init__(self, radius, ingredients):
+        self.radius = radius
+        self.ingredients = ingredients
+
+    def __repr__(self):
+        return (f'Pizza({self.radius!r}, ' f'{self.ingredients!r})')
+
+    def area(self):
+        return self.circle_area(self.radius)
+
+    @staticmethod
+    def circle_area(r):
+        return r ** 2 * math.pi
+
+    @classmethod
+    def margherita(cls):
+        return cls(['mozzarella', 'tomatoes'])
+
+    @classmethod
+    def prosciutto(cls):
+        return cls(['mozzarella', 'tomatoes', 'ham'])
