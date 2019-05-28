4f493ced3a0fae117407ab02fcc1ccba5f297183
JohnnySunkel <46906741+JohnnySunkel@users.noreply.github.com>
2019-05-28 10:28:09 -0500

    Create athlete_sort.py

---
+++ Basics/athlete_sort.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    rows = [input() for _ in range(n)]
+    k = int(input())
+    for row in sorted(rows, key = lambda row: int(row.split()[k])):
+        print(row)
