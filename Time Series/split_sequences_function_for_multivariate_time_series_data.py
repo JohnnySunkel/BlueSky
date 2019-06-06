ee796cf41173b8e02fe9c7f75f9cb7f3d327c2a4
JohnnySunkel <46906741+JohnnySunkel@users.noreply.github.com>
2019-06-06 15:51:22 -0500

    Update split_sequences_function_for_multivariate_time_series_data.py

--- Time Series/split_sequences_function_for_multivariate_time_series_data.py
+++ Time Series/split_sequences_function_for_multivariate_time_series_data.py
@@ -7,10 +7,10 @@ def split_sequences(sequences, n_steps):
         # Find the end of this pattern
         end_ix = i + n_steps
         # Check if we are beyond the dataset
-        if end_ix > len(sequences):
+        if end_ix > len(sequences) - 1:
             break
         # Gather input and output parts of the pattern
-        seq_x, seq_y = sequences[i: end_ix, :-1], sequences[end_ix - 1, -1]
+        seq_x, seq_y = sequences[i: end_ix, :-1], sequences[end_ix, -1]
         X.append(seq_x)
         y.append(seq_y)
     return array(X), array(y)
