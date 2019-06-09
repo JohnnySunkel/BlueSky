c6acfcfe4dc75fd20e8cc5e18810c0f1dee6cf24
JohnnySunkel <46906741+JohnnySunkel@users.noreply.github.com>
2019-06-08 19:49:24 -0500

    Create game_over.cpp

---
+++ Basics/game_over.cpp
@@ -0,0 +1,13 @@
+// Game Over 2.0
+// Demonstrates using declarations
+
+#include "pch.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "Game Over!" << endl; 
+	  return 0;
+}
