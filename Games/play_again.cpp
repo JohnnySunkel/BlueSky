41f1d78e4af4b042c37305c61f20f4c4190faef8
JohnnySunkel <46906741+JohnnySunkel@users.noreply.github.com>
2019-06-13 21:51:24 -0500

    Create play_again.cpp

---
+++ Games/play_again.cpp
@@ -0,0 +1,22 @@
+// Play Again
+// Demonstrates while loops
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char again = 'y';
+	while (again == 'y')
+	{
+		cout << "\n**Played an exciting game**";
+		cout << "\nDo you want to play again? (y/n): ";
+		cin >> again;
+	}
+	
+	cout << "\nOkay, bye.";
+
+	return 0;
+}
