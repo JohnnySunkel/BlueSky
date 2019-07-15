ba31724d98ad8261b04478e9ee9f34b1fb365b86
JohnnySunkel <46906741+JohnnySunkel@users.noreply.github.com>
2019-07-15 16:19:10 -0500

    Create prophet_forecasts_with_holidays_daily_total_upgrades.py

---
+++ Time Series/prophet_forecasts_with_holidays_daily_total_upgrades.py
@@ -0,0 +1,26 @@
+# Prophet forecasts of daily total upgrades
+import pandas as pd
+from fbprophet import Prophet
+
+# Load the dataset
+df = pd.read_csv('daily_total_upgrades_fbp.csv')
+df.head()
+
+# Define and fit the model
+m = Prophet()
+m.add_country_holidays(country_name = 'US')
+m.fit(df)
+
+# Create a dataframe to hold predictions
+future = m.make_future_dataframe(periods = 31)
+future.tail()
+
+# Make predictions
+forecast = m.predict(future)
+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(31)
+
+# Plot forecasts
+fig1 = m.plot(forecast)
+
+# Plot forecast components
+fig2 = m.plot_components(forecast)
