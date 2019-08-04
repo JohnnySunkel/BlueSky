19601f6ad227659e8d575909ed250729fc1c655b
JohnnySunkel <46906741+JohnnySunkel@users.noreply.github.com>
2019-08-04 09:11:56 -0500

    Create instructions.cpp

---
+++ Games/instructions.cpp
@@ -0,0 +1,23 @@
+// Instructions
+// Demonstrates writing new functions
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+// function prototype (declaration)
+void instructions();
+
+int main()
+{
+    instructions();
+	return 0;
+}
+
+// function definition
+void instructions()
+{
+	cout << "Welcome to the most fun you've ever had with text!\n\n";
+	cout << "Here's how to play the game...\n";
+}
