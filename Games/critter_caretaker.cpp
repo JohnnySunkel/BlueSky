87a8992c93baa51c91ce844dee99c42548cc41e0
JohnnySunkel <46906741+JohnnySunkel@users.noreply.github.com>
2019-08-28 08:54:30 -0500

    Update critter_caretaker.cpp

--- Games/critter_caretaker.cpp
+++ Games/critter_caretaker.cpp
@@ -38,9 +38,91 @@ void Critter::PassTime(int time)
 	m_Boredom += time;
 }
 
+void Critter::Talk()
+{
+	cout << "I'm a critter and I feel ";
 
+	int mood = GetMood();
+	if (mood > 15)
+	{
+		cout << "mad.\n";
+	}
+	else if (mood > 10)
+	{
+		cout << "frustrated.\n";
+	}
+	else if (mood > 5)
+	{
+		cout << "okay.\n";
+	}
+	else
+	{
+		cout << "happy.\n";
+	}
+
+	PassTime();
+}
+
+void Critter::Eat(int food)
+{
+	cout << "Burrppp.\n";
+	m_Hunger -= food;
+	if (m_Hunger < 0)
+	{
+		m_Hunger = 0;
+	}
+
+	PassTime();
+}
+
+void Critter::Play(int fun)
+{
+	cout << "Wheee!\n";
+
+	m_Boredom -= fun;
+	if (m_Boredom < 0)
+	{
+		m_Boredom = 0;
+	}
+
+	PassTime();
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    Critter crit;
+	crit.Talk();
+
+	int choice;
+	do
+	{
+		cout << "\nCritter Caretaker\n\n";
+		cout << "0 - Quit\n";
+		cout << "1 - Listen to your critter\n";
+		cout << "2 - Feed your critter\n";
+		cout << "3 - Play with your critter\n\n";
+
+		cout << "Choice: ";
+		cin >> choice;
+
+		switch (choice)
+		{
+		case 0:
+			cout << "Good-bye.\n";
+			break;
+		case 1:
+			crit.Talk();
+			break;
+		case 2:
+			crit.Eat();
+			break;
+		case 3:
+			crit.Play();
+			break;
+		default:
+			cout << "\nSorry, but " << choice << " isn't a valid choice.\n";
+		}
+	} while (choice != 0);
+
+	return 0;
 }
